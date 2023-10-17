73fa966e0cc5a535536fc4509dbe0c40a7756952
loopRW <loop.rw@gmail.com>
2023-10-17 22:43:53 +0200

    lab 3

--- Lab2/2.Eccezioni/1.FormatoOra/FormatoOrarioException.java
+++
@@ -1,9 +0,0 @@
-public class FormatoOrarioException extends Exception {
-	public FormatoOrarioException() {
-		super("Formato ora non valido");
-	}
-
-	public FormatoOrarioException(String messaggio) {
-		super(messaggio);
-	}
-}
